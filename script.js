ea26de9c3f4551a37e6213f3ca40c2d1899a85e6
Subha Maji <2022.subhamaji@gmail.com>
2025-07-03 11:40:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const countdown = () => {
+  const countDate = new Date("Jul 5, 2025 09:00:00").getTime();
+  const now = new Date().getTime();
+  const gap = countDate - now;
+
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const textDay = Math.floor(gap / day);
+  const textHour = Math.floor((gap % day) / hour);
+  const textMinute = Math.floor((gap % hour) / minute);
+  const textSecond = Math.floor((gap % minute) / second);
+
+  document.getElementById("days").innerText = textDay
+    .toString()
+    .padStart(2, "0");
+  document.getElementById("hours").innerText = textHour
+    .toString()
+    .padStart(2, "0");
+  document.getElementById("minutes").innerText = textMinute
+    .toString()
+    .padStart(2, "0");
+  document.getElementById("seconds").innerText = textSecond
+    .toString()
+    .padStart(2, "0");
+};
+
+setInterval(countdown, 1000);
