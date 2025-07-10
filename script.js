6d4f5afd0a532d97ad1177481e51f851bd590102
Subha Maji <2022.subhamaji@gmail.com>
2025-07-10 19:03:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-  const countDate = new Date("Jul 10, 2025 09:00:00").getTime();
+  const countDate = new Date("Jul 25, 2025 09:00:00").getTime();
   const now = new Date().getTime();
   const gap = countDate - now;
 
